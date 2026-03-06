d8249bebfa9190fbe15844b2673543f2e2be4b1b
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-03-06 22:56:21 +0600

    required link added,sign in page done ,sign In function added by js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
 console.log("My Name Is Shahadat Hossain");
+
+document.getElementById("signInBtn").addEventListener("click", function () {
+  console.log("Button clicked");
+
+  //!   input-user
+
+  const userInput = document.getElementById("input-user");
+  const user = userInput.value;
+  console.log(user);
+
+  //!   input-password
+
+  const inputPassword = document.getElementById("input-password");
+  const password = inputPassword.value;
+  console.log(password);
+
+  //!   Condition
+
+  if (user === "admin" && password === "admin123") {
+    alert("Successfully Signed In");
+    window.location.assign("./HomePage.html");
+  } else {
+    alert("Signing In Failed");
+    return;
+  }
+});
